c555e5c3c820f310bc4d0ae3f4c64bb240075ea1
cgee1 <christopherrgee@gmail.com>
2019-12-08 11:36:03 -0800

    Add delete function to word banks

--- src/components/options/options.jsx
+++ src/components/options/options.jsx
@@ -8,46 +8,68 @@ class Options extends React.Component {
             display: true,
             teamAName: 'Red Team',
             teamBName: 'Blue Team',
-            words: []
+            words: [],
+            showAnswers: false,
         }
+        this.index = 0;
     }
 
     handleInput(field) {
         return (e) => {
+            e.preventDefault();
             this.setState({ [field]: e.target.value });
         };
     }
 
     handleSubmitPlay() {
-        return () => {
-            this.setState({display: false})
-        }
+        this.setState({display: false})
     }
 
     addWord() {
         return (e) => {
             e.preventDefault();
-            this.setState({words: [...this.state.words, e.currentTarget[0].value]})
+            this.setState({ words: [...this.state.words, e.currentTarget[0].value]}, () => this.index++)
             e.currentTarget[0].value = '';
         }
     }
 
+    removeWord(idx) {
+        return () => {
+            let array = this.state.words;
+            array.splice(idx, 1)
+            this.setState({words: array});
+        }
+    }
+
+    displayAnswers() {
+        const { words, showAnswers } = this.state;
+        return (!showAnswers) ? 
+            <div className="answers-bank" onClick={this.toggleAnswers.bind(this)}>Show Answer Bank</div> :
+            <div className="answers-bank">Answers: 
+                {words.map((word, i) => <div className="answers-bank_word" key={i} onClick={this.removeWord(i).bind(this)}>{word}</div>).reverse()}
+            </div>
+    }
+
+    toggleAnswers() {
+        this.setState({showAnswers: !this.state.showAnswers});
+    }
+
     displayOptions() {
-        const { display, words } = this.state;
+        const { display } = this.state;
         if (!display) return null;
 
         return (
             <div className="options-display">
                 <form className="options-form " onSubmit={this.addWord().bind(this)}>
-                    <input type="text" className="options-input" name="word" placeholder="Add to the answers" />
+                    <input type="text" className="options_input" name="word" placeholder="Add to the answers" />
                     <input type="submit" className="btn" value="Add Word/Phrase"/> 
                 </form>
-                <form className="options-form team-names" onSubmit={this.handleSubmitPlay().bind(this)}>
-                    <label className="btn-label">Team 1's Name: </label>
-                    <input type="text" className="options-input" placeholder="Red Team" onChange={this.handleInput('teamAName').bind(this)} />
-                    <label className="btn-label">Team 2's Name: </label>
-                    <input type="text" className="options-input" placeholder="Blue Team" onChange={this.handleInput('teamBName').bind(this)} />
-                    <div>{words.join(', ')}</div>
+                <form className="options-form team-names" onSubmit={this.handleSubmitPlay.bind(this)}>
+                    <label className="btn_label">Team 1's Name: </label>
+                    <input type="text" className="options_input" placeholder="Red Team" onChange={this.handleInput('teamAName').bind(this)} />
+                    <label className="btn_label">Team 2's Name: </label>
+                    <input type="text" className="options_input" placeholder="Blue Team" onChange={this.handleInput('teamBName').bind(this)} />
+                    <div className="answers-bank_container">{this.displayAnswers()}</div>
                     <input type="submit" className="btn" value="Start Game"/>
                 </form>
             </div>
