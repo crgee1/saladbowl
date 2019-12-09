a19352f92be78aec75d13ecfa03b1f75ea1a1bf5
cgee1 <christopherrgee@gmail.com>
2019-12-08 23:16:58 -0800

    Edit end screen

--- src/components/modals/end_screen.jsx
+++ src/components/modals/end_screen.jsx
@@ -10,11 +10,21 @@ export default class endScreen extends React.Component {
     }
 
     render() {
-
+        const { teamA, teamB } = this.props;
         return (
             <div className="end-screen-container">
                 <div className="round-screen">
                     <h3>{this.winner()}</h3>
+                    <div className="round-screen-scores">
+                        <div>
+                            <div>Team: {teamA.name}</div>
+                            <div>Points: {teamA.points}</div>
+                        </div>
+                        <div>
+                            <div>Team: {teamB.name}</div>
+                            <div>Points: {teamB.points}</div>
+                        </div>
+                    </div>
                 </div>
                 <div className="end-screen-btn-container">
                     <div className="btn round-screen-btn"><Link to={'/'}>Back to Menu</Link></div>
