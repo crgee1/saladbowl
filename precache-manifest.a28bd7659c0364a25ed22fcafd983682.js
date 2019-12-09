bc1a8f9143f5bb00ca043d023760bb5f033b0d6d
cgee1 <christopherrgee@gmail.com>
2019-12-08 22:27:06 -0800

    Updates

---
+++ precache-manifest.a28bd7659c0364a25ed22fcafd983682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c0d236d57d3bbe48d8af1aa3b65cb0",
+    "url": "/crgee1/saladbowl/index.html"
+  },
+  {
+    "revision": "15261d4f20788714d7a7",
+    "url": "/crgee1/saladbowl/static/css/main.61141bd1.chunk.css"
+  },
+  {
+    "revision": "fd5605368d87ad8d3449",
+    "url": "/crgee1/saladbowl/static/js/2.cbe0208a.chunk.js"
+  },
+  {
+    "revision": "15261d4f20788714d7a7",
+    "url": "/crgee1/saladbowl/static/js/main.d07a3b69.chunk.js"
+  },
+  {
+    "revision": "19c03bcb4dcb2c394d8d",
+    "url": "/crgee1/saladbowl/static/js/runtime-main.2e9c0b6e.js"
+  },
+  {
+    "revision": "2306fee08ac5c4ee2cb2f3188ca578e3",
+    "url": "/crgee1/saladbowl/static/media/saladbowl.2306fee0.png"
+  }
+]);
