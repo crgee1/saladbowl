efaec8980336e18049c7243b7c2dde03263e23b8
cgee1 <christopherrgee@gmail.com>
2019-12-09 12:33:06 -0800

    Start how to page

--- src/components/how_to/how_to.jsx
+++ src/components/how_to/how_to.jsx
@@ -4,7 +4,9 @@ export default class extends React.Component {
     render() {
         return (
         <div className="how-to">
-            sadas
+            1. Fill the answers bank. Every player should add the same amount of answers (Suggested: 3 - 7 answers per player). Answers can be anything from nouns to common expressions (Suggested: Limit it to 5 words per answer; Try not to pick something too obscure for the player base). <br/>
+            2. Split into two teams
+            
         </div>
         )
     }
