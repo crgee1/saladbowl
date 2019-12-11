d507b730c7353217b2475ce4389ca0d5846b1eda
cgee1 <christopherrgee@gmail.com>
2019-12-11 14:24:55 -0800

    Switch modal to functional component

--- src/components/modals/modal.jsx
+++ src/components/modals/modal.jsx
@@ -2,47 +2,90 @@ import React from 'react';
 import RoundScreen from './round_screen';
 import EndScreen from './end_screen';
 
-class Modal extends React.Component {
+// class Modal extends React.Component {
 
-    render() {
-        const { modal, teamA, teamB, startRound, playingTeam, round, toggleInput } = this.props;
-        if (!modal) {
-            return null;
-        }
+//     render() {
+//         const { modal, teamA, teamB, startRound, playingTeam, round, toggleInput } = this.props;
+//         if (!modal) {
+//             return null;
+//         }
 
-        switch (modal) {
-            case 'roundScreen':
-                this.component = 
-                    <RoundScreen 
-                        teamA={teamA} 
-                        teamB={teamB} 
-                        startRound={startRound} 
-                        playingTeam={playingTeam} 
-                        round={round} 
-                        toggleInput={toggleInput}
-                    />
-                break;
-            case 'endScreen':
-                this.component = 
-                    <EndScreen 
-                        teamA={teamA} 
-                        teamB={teamB} 
-                        startRound={startRound} 
-                        playingTeam={playingTeam}
-                    />
-                break;
-            default:
-                return null;
-        }
+//         switch (modal) {
+//             case 'roundScreen':
+//                 this.component = 
+//                     <RoundScreen 
+//                         teamA={teamA} 
+//                         teamB={teamB} 
+//                         startRound={startRound} 
+//                         playingTeam={playingTeam} 
+//                         round={round} 
+//                         toggleInput={toggleInput}
+//                     />
+//                 break;
+//             case 'endScreen':
+//                 this.component = 
+//                     <EndScreen 
+//                         teamA={teamA} 
+//                         teamB={teamB} 
+//                         startRound={startRound} 
+//                         playingTeam={playingTeam}
+//                     />
+//                 break;
+//             default:
+//                 return null;
+//         }
 
-        return (
-            <div className="modal-background">
-                <div className="modal-child" onClick={e => e.stopPropagation()}>
-                    {this.component}
-                </div>
-            </div>
-        );
+//         return (
+//             <div className="modal-background">
+//                 <div className="modal-child" onClick={e => e.stopPropagation()}>
+//                     {this.component}
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+function Modal(props) {
+    const { modal, teamA, teamB, startRound, playingTeam, round, toggleInput } = props;
+        
+    if (!modal) {
+        return null;
     }
+
+    let component;
+
+    switch (modal) {
+        case 'roundScreen':
+            component = 
+                <RoundScreen 
+                    teamA={teamA} 
+                    teamB={teamB} 
+                    startRound={startRound} 
+                    playingTeam={playingTeam} 
+                    round={round} 
+                    toggleInput={toggleInput}
+                />
+            break;
+        case 'endScreen':
+            component = 
+                <EndScreen 
+                    teamA={teamA} 
+                    teamB={teamB} 
+                    startRound={startRound} 
+                    playingTeam={playingTeam}
+                />
+            break;
+        default:
+            return null;
+    }
+
+    return (
+        <div className="modal-background">
+            <div className="modal-child" onClick={e => e.stopPropagation()}>
+                {component}
+            </div>
+        </div>
+    );
 }
 
 export default Modal;
