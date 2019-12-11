ef0645a220ed77517e3c7af1a436a9ba90885794
cgee1 <christopherrgee@gmail.com>
2019-12-11 14:37:07 -0800

    Implemented end screen with hooks

--- src/components/modals/end_screen.jsx
+++ src/components/modals/end_screen.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class endScreen extends React.Component {
+export default function endScreen(props) {
 
-    winner() {
-        const { teamA, teamB } = this.props;
+    const winner = () => {
+        const { teamA, teamB } = props;
 
         return teamA.points === teamB.points ? 'It is a Draw!' : teamA.points > teamB.points ? `${teamA.name} Wins!` : `${teamB.name} Wins!`;
     }
 
-    render() {
-        const { teamA, teamB } = this.props;
-        return (
-            <div className="end-screen-container">
-                <div className="round-screen">
-                    <h3>{this.winner()}</h3>
-                    <div className="round-screen-scores">
-                        <div>
-                            <div>Team: {teamA.name}</div>
-                            <div>Points: {teamA.points}</div>
-                        </div>
-                        <div>
-                            <div>Team: {teamB.name}</div>
-                            <div>Points: {teamB.points}</div>
-                        </div>
+    const { teamA, teamB } = props;
+    
+    return (
+        <div className="end-screen-container">
+            <div className="round-screen">
+                <h3>{winner()}</h3>
+                <div className="round-screen-scores">
+                    <div>
+                        <div>Team: {teamA.name}</div>
+                        <div>Points: {teamA.points}</div>
+                    </div>
+                    <div>
+                        <div>Team: {teamB.name}</div>
+                        <div>Points: {teamB.points}</div>
                     </div>
                 </div>
-                <div className="end-screen-btn-container">
-                    <Link to={'/'}><div className="btn round-screen-btn">Back to Menu</div></Link>
-                </div>
             </div>
-        )
-    }
+            <div className="end-screen-btn-container">
+                <Link to={'/'}><div className="btn round-screen-btn">Back to Menu</div></Link>
+            </div>
+        </div>
+    )
 }
